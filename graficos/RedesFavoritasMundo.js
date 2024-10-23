c1db05ff025f0162008487b3316b9c779b8a2246
Sâmela <104445773+sasahvicentee@users.noreply.github.com>
2024-10-23 08:28:08 -0300

    ajustes

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -1,47 +1,50 @@
-import { getCSS, criarGrafico, incluirTexto } from "./common.js"
-
+import { getCSS } from "./common.js";
 
 async function redesFavoritasMundo() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const redes = Object.keys(dados)
-    const valores = Object.values(dados)
+  const url =
+    "https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json";
+  const res = await fetch(url);
+  const dados = await res.json();
+  const redes = Object.keys(dados);
+  const valores = Object.values(dados);
 
-    const data = [
-        {
-            values: valores,
-            labels: redes,
-            type: 'pie',
-            textinfo: 'label+percent'
-        }
-    ]
+  // Criar o gráfico de pizza
+  const data = [
+    {
+      values: valores,
+      labels: redes,
+      type: "pie",
+      textinfo: "label+percent",
+    },
+  ];
 
-    const layout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        height: 700,
-        title: {
-            text: 'Redes sociais que os usuários mais gostam',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                family: getCSS('--font'),
-                size: 30
-            }
-        },
-        legend: {
-            font: {
-                color: getCSS('--primary-color'),
-                size: 16
-            }
-        }
-    }
+  // Configurar o layout do gráfico
 
-    criarGrafico(data, layout)
+  const layout = {
+    plot_bgcolor: getCSS("--bg-color"),
+    paper_bgcolor: getCSS("--bg-color"),
+    title: {
+      text: "Redes sociais que os usuários mais gostam",
+      x: 0,
+      font: {
+        color: getCSS("--primary-color"),
+        family: getCSS("--font"),
+        size: 30,
+      },
+    },
+    legend: {
+      font: {
+        color: getCSS("--primary-color"),
+        size: 16,
+      },
+    },
+  };
 
-    incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários entre as redes sociais, destaca-se como a <span>preferida pelos usuários</ span>. Supera até mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais`)
+  // Inserir o gráfico na página
+  const grafico = document.createElement("div");
+  grafico.className = "grafico";
+  document.getElementById("graficos-container").appendChild(grafico);
+  Plotly.newPlot(grafico, data, layout);
 }
 
-
-redesFavoritasMundo()
+redesFavoritasMundo();
