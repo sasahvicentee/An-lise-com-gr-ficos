c0116db7ad5ee45fe6e766addfa72ea05545e913
Sâmela <104445773+sasahvicentee@users.noreply.github.com>
2024-10-23 08:36:58 -0300

    z

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -53,6 +53,6 @@ async function quantidadeUsuariosPorRede() {
     const grafico = document.createElement("div");
     grafico.className = "grafico";
     document.getElementById("graficos-container").appendChild(grafico);
-    Plotly.newPlot(grafico, data, layout);
-    
+    Plotly.newPlot(grafico, data, layout); }
+
 quantidadeUsuariosPorRede()
