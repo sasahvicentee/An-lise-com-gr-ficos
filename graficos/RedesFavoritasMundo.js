c7d03b70b7622f99e0e6334736bf7a39a2d236d3
Sâmela <104445773+sasahvicentee@users.noreply.github.com>
2024-10-30 07:44:04 -0300

    alterações

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -1,11 +1,11 @@
-import { getCSS } from "./common.js"
+import { getCSS, criarGrafico } from "./common.js"
 
 async function redesFavoritasMundo() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
     const res = await fetch(url)
     const dados = await res.json()
     const redes = Object.keys(dados)
-    const valores = Object.values(dados);
+    const valores = Object.values(dados)
 
     const data = [
         {
@@ -16,10 +16,10 @@ async function redesFavoritasMundo() {
         }
     ]
 
-
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
         title: {
             text: 'Redes sociais que os usuários mais gostam',
             x: 0,
@@ -34,13 +34,10 @@ async function redesFavoritasMundo() {
                 color: getCSS('--primary-color'),
                 size: 16
             }
+        }
     }
-}
 
-const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+    criarGrafico(data, layout)
 }
 
 redesFavoritasMundo()
